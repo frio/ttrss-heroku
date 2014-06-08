b09a2307d0b6cd2d5962a2a599edd2e52f570616
frio <development@frio.name>
2014-06-08 20:12:16 +1200

    Changed key for APP_URL to make it more readable

--- config.php
+++ config.php
@@ -21,7 +21,7 @@
 	// *** Basic settings (important!) ***
 	// ***********************************
 
-	define('SELF_URL_PATH', $_ENV['URL_PATH']);
+	define('SELF_URL_PATH', $_ENV['APP_URL']);
 	// Full URL of your tt-rss installation. This should be set to the
 	// location of tt-rss directory, e.g. http://example.org/tt-rss/
 	// You need to set this option correctly otherwise several features
